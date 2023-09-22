6364c0a5dfad12d2690d71a5f014fe8286e5b609
nathanpage <nathan.page@eonx.com>
2023-09-22 13:50:30 +1000

    Resolve request in cli for bugsnag

--- public/index_lambda.php
+++ public/index_lambda.php
@@ -2,9 +2,13 @@
 
 use App\Infrastructure\Bref\SymfonyHttpHandler;
 use App\Infrastructure\HttpKernel\Kernel;
+use EonX\EasyBugsnag\Interfaces\ValueOptionInterface as EasyBugsnagValueOptionInterface;
 use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+// Because of serverless it looks to PHP that it's running in CLI
+$_SERVER[EasyBugsnagValueOptionInterface::RESOLVE_REQUEST_IN_CLI] = true;
 
 (new Dotenv())
     ->setProdEnvs(['prod'])
